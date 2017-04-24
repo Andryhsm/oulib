cb79af7ede44a7facee954c326f0271d8ddfe113
ffnn <fenoheriniainat@gmail.com>
2017-04-24 09:25:12 +0200

    premier commit

---
+++ lib-php/cnx - Copie.php
@@ -0,0 +1,10 @@
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=imedsoftcibast.mysql.db;dbname=imedsoftcibast', 'imedsoftcibast', '1groupeDrn');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
